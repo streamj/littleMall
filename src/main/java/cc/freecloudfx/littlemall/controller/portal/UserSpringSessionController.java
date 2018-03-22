3298fa75608bce01d64e11665492c93e3c8648cf
streamj <mystream@vip.qq.com>
2018-03-22 15:35:59 +0800

    optional Srping Session

---
+++ src/main/java/cc/freecloudfx/littlemall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,62 @@
+package cc.freecloudfx.littlemall.controller.portal;
+
+import cc.freecloudfx.littlemall.common.Const;
+import cc.freecloudfx.littlemall.common.ServerResponse;
+import cc.freecloudfx.littlemall.pojo.User;
+import cc.freecloudfx.littlemall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author StReaM on 2/24/2018
+ */
+@Controller
+@RequestMapping("/user/springsession/")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService iUserService;
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody() /*返回值自动使用 Jackson 序列化*/
+    public ServerResponse<User> login(String username, String password,
+                                      HttpSession session) {
+        // service -> mybatis -> dao
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @ResponseBody()
+    public ServerResponse<String> logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+
+        return ServerResponse.createBySuccess();
+    }
+
+
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
+    @ResponseBody()
+    public ServerResponse<User> getUserInfo(HttpSession session) {
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+
+        if (user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取信息");
+    }
+}
